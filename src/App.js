51f51c583c0843925603c598a004d3794fde947a
dhehwa84 <dhehwa84@gmail.com>
2022-07-25 15:07:57 +0200

    multiple components
    -added multiple comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
-import './App.css';
 
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
-    const title = 'Welcome to the new blog';
-    const likes = 50;
-    const link = "http://www.google.com"
   return (
     <div className="App">
+        <Navbar />
      <div className="content">
-       
+         <Home />
 
      </div>
     </div>
