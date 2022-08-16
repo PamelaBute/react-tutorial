e0d4e6267ecaa2947dc17469dd27ad08e4b459d5
PamelaBute <pamelabute02@gmail.com>
2022-08-16 14:01:51 +0200

    react-functions-as-props
     - can use function as props
     - added a delete function on the blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,5 @@
-const BlogList = (props) =>{
-    const blogs = props.blogs; 
-    const title = props.title;
-
-    console.log(props,blogs);
+const BlogList = ({blogs, title, handleDelete}) =>{
+   
 return (
    <div className="blog-list">
     <h2> {title}</h2>
@@ -10,6 +7,8 @@ return (
             <div className="blog-preview" key= {blog.id}>
                 <h2>{blogs.title}</h2>
                 <p> written by  { blog.author}</p>
+                <button onClick ={() =>handleDelete(blog.id)}> delete blog </button>
+               
                 </div>
           ))}
    </div> 
--- src/Home.js
+++ src/Home.js
@@ -13,11 +13,14 @@ const Home = ( ) =>{
     // const handleClick = () => {
     //    setName('lungi');
     //    setAge(30);
-  
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog =>blog.id !==id);
+        setBlogs(newBlogs);
+    }
    
     return (
         <div className= "home">
-          <BlogList  blogs ={blogs} title = "All blogs!"/>
+          <BlogList  blogs ={blogs} title = "All blogs!" handleDelete ={handleDelete}/>
           <BlogList  blogs ={blogs.filter((blog)=> blog.author === 'simon')} title = "simon`s blogs"/>
         </div>
     );
