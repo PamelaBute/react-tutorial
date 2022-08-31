c22b3f77034c4f1ad5bd0bf5e664e6d9cba923dc
PamelaBute <pamelabute02@gmail.com>
2022-08-31 13:31:12 +0200

    Update Home.js
    - use effect dependency

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,9 @@ const Home = ( ) =>{
         {title: 'My new website', body: 'lorem ipsum...', author:'pamela', id: 1},
         {title: 'Welcome party!', body: 'lorem ipsum...', author:'simon', id: 2},
         {title: 'Web dev top tips', body: 'lorem ipsum...', author:'Simone', id: 3}
-     ])
+     ]);
+     const [name, setName]= useState('pamela');
+
     // let name = 'mario';
     // const[name, setName]= useState( 'mario');
     // const[age, setAge]= useState(25);
@@ -19,12 +21,15 @@ const Home = ( ) =>{
     }
     useEffect(() =>{
         console.log('use effect ran');
-    })
+        console.log(blogs);
+    },[]);
    
     return (
         <div className= "home">
           <BlogList  blogs ={blogs} title = "All blogs!" handleDelete ={handleDelete}/>
-          <BlogList  blogs ={blogs.filter((blog)=> blog.author === 'simon')} title = "simon`s blogs"/>
+           <button onClick={() => setName('luigi')}> change name </button>
+           <p>{ name}</p>
+
         </div>
     );
 }
